12456d79f0c2cd08779f5a13d14e8b2ccf14d1de
Nguyen1397 <knguyen4582@gmail.com>
2023-10-23 10:36:15 +0700

    Update nav.js

--- JavaScript/gia-sp&thoi-gian/js/nav.js
+++ JavaScript/gia-sp&thoi-gian/js/nav.js
@@ -3,16 +3,27 @@ const don_2 = document.getElementById('don2');
 const don_3 = document.getElementById('don3');
 
 // Nội dung đơn hàng 1
-var don1 = {
-    gia: 30,
-    so_luong: 3,
-    tong: gia * so_luong,
-    tinh:function() {
-        document.write('Gia: ' + this.gia + '<br>');
+var don1 = function() {
+    document.write('<h2>Don 1: </h2>');
+    var gia = 31;
+    var so_luong = 3;
+    var tong = gia * so_luong;
+    var giam_gia;
+    document.write('<p>Gia: ' + gia + '<br>');
+    document.write('So luong: ' + so_luong + '<br>');
+    document.write('Tong: ' + tong + '<br>');
+    if (tong > 100) {
+        giam_gia = 5;
+        cls_name = 'co';
     }
+    else {
+        giam_gia = 0;
+        cls_name = 'ko';
+    }
+    document.write('Giam gia: ' + giam_gia + '%</p>')
 }
-don_1.textContent = don1.tinh;
-don_1.className = 'ko';
+don_1.textContent = don1();
+don_1.className = cls_name;
 
 // Nội dung đơn hàng 2
 
